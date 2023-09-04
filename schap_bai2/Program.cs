509e819a9ddf1941526478d1841fdf9c1c34806b
ADMIN <ADMIN@LAB1-MAY29>
2023-09-04 14:16:47 +0700

    Add project files.

---
+++ schap_bai2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schap_bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nhap ten cua ban");
+            string ten = Console.ReadLine ();
+            Console.WriteLine("Hello {0}!,ten");
+            Console.ReadKey ();
+        }
+    }
+}
